a8bbc1479c0f742b56b25f19c166277a40a43cb6
thezmmm <1563081082@qq.com>
2025-12-02 16:40:08 +0200

    feat: xgb 2-classifier and 4-classifier

---
+++ XGBoost.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report, confusion_matrix
+import xgboost as xgb
+from preprocess import preprocess_data
+
+def train_four_class(X,y):
+    # split train and test set
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.05, random_state=42, stratify=y
+    )
+
+    # initialize XGBoost multi-classifier
+    clf = xgb.XGBClassifier(
+        objective='multi:softprob',  # output percentage
+        num_class=len(np.unique(y)),
+        eval_metric='mlogloss',
+        use_label_encoder=False,
+        n_estimators=200,
+        max_depth=6,
+        learning_rate=0.06,
+        subsample=0.8,
+        colsample_bytree=0.8,
+        random_state=42
+    )
+
+    # train
+    clf.fit(X_train, y_train)
+
+    # predict
+    y_pred = clf.predict(X_test)
+    # eval
+    print("Classification Report:\n")
+    print(classification_report(y_test, y_pred))
+    print("\nConfusion Matrix:\n")
+    print(confusion_matrix(y_test, y_pred))
+
+    return clf
+
+
+def train_two_class(X,y):
+    # split train and test set
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.05, random_state=42, stratify=y
+    )
+
+    clf = xgb.XGBClassifier(
+        objective='binary:logistic',
+        eval_metric='logloss',
+        n_estimators=200,
+        max_depth=6,
+        learning_rate=0.06,
+        subsample=0.8,
+        colsample_bytree=0.8,
+        random_state=42
+    )
+
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+    y_prob = clf.predict_proba(X_test)[:, 1]
+
+    print("Classification Report:\n")
+    print(classification_report(y_test, y_pred))
+    print("\nConfusion Matrix:\n")
+    print(confusion_matrix(y_test, y_pred))
+
+    return clf
+
+if __name__ == "__main__":
+    df, le = preprocess_data("./data/train.csv")
+    # spilt feature and label
+    X = df.drop("class4", axis=1)
+    y = df["class4"]
+    clf_4 = train_four_class(X,y)
+
+    y_orig = le.inverse_transform(df['class4'])
+    y = np.array([0 if label == 'nonevent' else 1 for label in y_orig])
+    clf_2 = train_two_class(X,y)
